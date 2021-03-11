947a269285a1e545c679ffbe96c2f04b3c7d9e86
kojimadaiki <swim1205@ezweb.ne.jp>
2021-03-11 09:55:16 +0900

    トップページ編集

--- resources/views/practice/top.blade.php
+++ resources/views/practice/top.blade.php
@@ -2,15 +2,17 @@
 @section('title', 'トップページ')
 @section('content')
 <div class="main inner">
-  <div class="top-sentence">
-    <h2>日々の練習メニューを保存しましょう</h2>
-    <h4>ログインするだけで誰でも簡単に残しておくことができます</h4>
-  </div>
-  <div class="make">
-    <p><a href="{{ url('/post') }}">メニューを保存する</a></p>
-  </div>
-  <div class="watch">
-    <p><a href="{{ url('/watch') }}">保存したメニューを見る</a></p>
+  <div class="main-bg">
+    <div class="top-sentence">
+      <h2>日々の練習メニューを保存しましょう</h2>
+      <h4>ログインするだけで誰でも簡単に残しておくことができます</h4>
+    </div>
+    <div class="make">
+      <p><a href="{{ url('/post') }}">メニューを保存する</a></p>
+    </div>
+    <div class="watch">
+      <p><a href="{{ url('/watch') }}">保存したメニューを見る</a></p>
+    </div>
   </div>
 </div>
 <script>
