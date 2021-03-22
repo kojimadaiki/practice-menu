8003f69435bc5c865b876c7ee127d2188f352062
kojimadaiki <swim1205@ezweb.ne.jp>
2021-03-22 09:33:11 +0900

    menuリクエスト作成

---
+++ app/Http/Requests/MenuRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MenuRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required | max:100', // Swim, Kick, Pull, W-up
+            'style' => 'required', // 種目
+            'times' => 'required', // 本数
+            'strength' => 'required', // 強度・練習内容
+            'long' => 'required', // 距離
+            'time' => 'required', // サークル
+            'total' => 'required', // トータル
+            'impression' => 'required', // 感想・反省
+        ];
+    }
+}
