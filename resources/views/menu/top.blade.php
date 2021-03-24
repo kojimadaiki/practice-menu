4c125aa9d27a7d2d1f6a24c67f802a6de048c46a
kojimadaiki <swim1205@ezweb.ne.jp>
2021-03-24 23:51:44 +0900

    トップページ編集

--- resources/views/menu/top.blade.php
+++ resources/views/menu/top.blade.php
@@ -4,15 +4,28 @@
 <div class="main inner">
   <div class="main-bg">
     <div class="top-sentence">
-      <h2>日々の練習メニューを保存しましょう</h2>
-      <h4>ログインするだけで誰でも簡単に残しておくことができます</h4>
+      <h2>日々の練習メニューを保存しよう！</h2>
+      <h4>ログインするだけで誰でも簡単に残すことができます</h4>
     </div>
-    <div class="make">
-      <p><a href="{{ url('/post') }}">メニューを保存する</a></p>
+    @guest
+    <div class="login-signin">
+      <div class="user-login">
+        <p><a class="nav-link" href="{{ route('login') }}">{{ __('ログインする') }}</a></p>
+      </div>
+      <div class="user-signin">
+        <p><a class="nav-link" href="{{ route('register') }}">{{ __('サインインする') }}</a></p>
+      </div>
     </div>
-    <div class="watch">
-      <p><a href="{{ url('/watch') }}">保存したメニューを見る</a></p>
+    @else
+    <div class="make-watch">
+      <div class="make">
+        <p><a href="{{ url('/post') }}">メニューを保存する</a></p>
+      </div>
+      <div class="watch">
+        <p><a href="{{ url('/watch') }}">保存したメニューを見る</a></p>
+      </div>
     </div>
+    @endguest
   </div>
 </div>
 @endsection
