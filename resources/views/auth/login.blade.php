3134568f6e775dc253c0eebc6c49d5a908152f76
kojimadaiki <swim1205@ezweb.ne.jp>
2021-03-24 23:50:05 +0900

    ログインビュー編集

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="login">
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">{{ __('Login') }}</div>
 
         <div class="card-body">
           <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <div class="form-group row email">
+              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
-              <div class="col-md-6">
+              <div class="col-md-6 mail">
                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                 @error('email')
@@ -25,8 +24,8 @@
               </div>
             </div>
 
-            <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+            <div class="form-group row password">
+              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
               <div class="col-md-6">
                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -41,8 +40,8 @@
 
             <div class="form-group row mb-0">
               <div class="col-md-8 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                  {{ __('Login') }}
+                <button type="submit" class="btn btn-primary btn-login">
+                  {{ __('ログイン') }}
                 </button>
               </div>
             </div>
